40fb2b2f86fa56eaaf44dbfef5816391128d4da4
Christopher P. Maher <chris@mahercode.io>
2018-02-04 21:56:34 -0800

    More testing

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,1 @@
-node.default['openshift']['url'] = 'https://github.com/openshift/origin/releases/download/v3.7.1/openshift-origin-server-v3.7.1-ab0f056-linux-64bit.tar.gz'
+default['openshift']['url'] = 'https://github.com/openshift/origin/releases/download/v3.7.1/openshift-origin-server-v3.7.1-ab0f056-linux-64bit.tar.gz'
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,13 +4,14 @@
 #
 # Copyright:: 2018, Christopher P. Maher, MIT.
 
-# tar_extract "#{node['openshift']['url']}" do
-# 	target_dir "/"
-# 	creates '/openshift'
-# end
+tar_extract "https://github.com/openshift/origin/releases/download/v3.7.1/openshift-origin-server-v3.7.1-ab0f056-linux-64bit.tar.gz" do
+  target_dir '/opt'
+  creates '/opt/openshift-origin-server-v3.7.1-ab0f056-linux-64bit'
+end
 
 package 'docker'
 
 service 'docker' do
   action [:start, :enable]
 end
+
